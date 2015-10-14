8a44eb81bda45b7817e963afab408e0202ffb5cc
sboikov <sboikov@gridgain.com>
2015-10-14 18:01:29 +0300

    ignite-1607 WIP

--- modules/core/src/test/java/org/apache/ignite/testsuites/IgniteBasicTestSuite.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/IgniteBasicTestSuite.java
@@ -36,6 +36,7 @@ import org.apache.ignite.internal.processors.affinity.GridAffinityProcessorRende
 import org.apache.ignite.internal.processors.cache.GridProjectionForCachesOnDaemonNodeSelfTest;
 import org.apache.ignite.internal.processors.cache.IgniteDaemonNodeMarshallerCacheTest;
 import org.apache.ignite.internal.processors.cache.OffHeapTieredTransactionSelfTest;
+import org.apache.ignite.internal.processors.cache.SerializableTransactionsLockTest;
 import org.apache.ignite.internal.processors.closure.GridClosureProcessorSelfTest;
 import org.apache.ignite.internal.processors.continuous.GridEventConsumeSelfTest;
 import org.apache.ignite.internal.processors.continuous.GridMessageListenSelfTest;
@@ -68,48 +69,10 @@ public class IgniteBasicTestSuite extends TestSuite {
     public static TestSuite suite(Set<Class> ignoredTests) throws Exception {
         TestSuite suite = new TestSuite("Ignite Basic Test Suite");
 
-        suite.addTest(IgniteLangSelfTestSuite.suite());
-        suite.addTest(IgniteUtilSelfTestSuite.suite(ignoredTests));
-        suite.addTest(IgniteMarshallerSelfTestSuite.suite(ignoredTests));
-
-        suite.addTest(IgniteKernalSelfTestSuite.suite(ignoredTests));
-        suite.addTest(IgniteStartUpTestSuite.suite());
-        suite.addTest(IgniteExternalizableSelfTestSuite.suite());
-        suite.addTest(IgniteP2PSelfTestSuite.suite());
-        suite.addTest(IgniteCacheP2pUnmarshallingErrorTestSuite.suite(ignoredTests));
-        suite.addTest(IgniteStreamSelfTestSuite.suite());
-
-        suite.addTest(new TestSuite(GridSelfTest.class));
-        suite.addTest(new TestSuite(ClusterGroupHostsSelfTest.class));
-        suite.addTest(new TestSuite(IgniteMessagingWithClientTest.class));
-
-        GridTestUtils.addTestIfNeeded(suite, ClusterGroupSelfTest.class, ignoredTests);
-        GridTestUtils.addTestIfNeeded(suite, GridMessagingSelfTest.class, ignoredTests);
-        GridTestUtils.addTestIfNeeded(suite, GridMessagingNoPeerClassLoadingSelfTest.class, ignoredTests);
-
-        if (U.isLinux() || U.isMacOs())
-            suite.addTest(IgniteIpcSharedMemorySelfTestSuite.suite());
-
-        GridTestUtils.addTestIfNeeded(suite, GridReleaseTypeSelfTest.class, ignoredTests);
-        suite.addTestSuite(GridProductVersionSelfTest.class);
-        suite.addTestSuite(GridAffinityProcessorRendezvousSelfTest.class);
-        suite.addTestSuite(GridClosureProcessorSelfTest.class);
-        suite.addTestSuite(ClosureServiceClientsNodesTest.class);
-        suite.addTestSuite(GridStartStopSelfTest.class);
-        suite.addTestSuite(GridProjectionForCachesSelfTest.class);
-        suite.addTestSuite(GridProjectionForCachesOnDaemonNodeSelfTest.class);
-        suite.addTestSuite(GridSpiLocalHostInjectionTest.class);
-        suite.addTestSuite(GridLifecycleBeanSelfTest.class);
-        suite.addTestSuite(GridStopWithCancelSelfTest.class);
-        suite.addTestSuite(GridReduceSelfTest.class);
-        suite.addTestSuite(GridEventConsumeSelfTest.class);
-        suite.addTestSuite(GridSuppressedExceptionSelfTest.class);
-        suite.addTestSuite(GridLifecycleAwareSelfTest.class);
-        suite.addTestSuite(GridMessageListenSelfTest.class);
-        suite.addTestSuite(GridFailFastNodeFailureDetectionSelfTest.class);
-        suite.addTestSuite(OffHeapTieredTransactionSelfTest.class);
-        suite.addTestSuite(IgniteSlowClientDetectionSelfTest.class);
-        suite.addTestSuite(IgniteDaemonNodeMarshallerCacheTest.class);
+        suite.addTestSuite(SerializableTransactionsLockTest.class);
+        suite.addTestSuite(SerializableTransactionsLockTest.class);
+        suite.addTestSuite(SerializableTransactionsLockTest.class);
+        suite.addTestSuite(SerializableTransactionsLockTest.class);
 
         return suite;
     }
