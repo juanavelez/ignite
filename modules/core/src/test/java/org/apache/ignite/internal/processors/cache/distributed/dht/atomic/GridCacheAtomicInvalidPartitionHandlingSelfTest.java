57b44cc265151a083c45298835bc42def3494854
Anton Vinogradov <av@apache.org>
2015-10-23 10:52:02 +0300

    1093

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/distributed/dht/atomic/GridCacheAtomicInvalidPartitionHandlingSelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/distributed/dht/atomic/GridCacheAtomicInvalidPartitionHandlingSelfTest.java
@@ -105,6 +105,9 @@ public class GridCacheAtomicInvalidPartitionHandlingSelfTest extends GridCommonA
 
         GridTestUtils.setMemoryMode(cfg, ccfg, memMode, 100, 1024);
 
+        //To be uncommented after https://issues.apache.org/jira/browse/IGNITE-1578 fix.
+        ((TcpCommunicationSpi)cfg.getCommunicationSpi()).setSharedMemoryPort(-1);
+
         return cfg;
     }
 
