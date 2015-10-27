b1eb21c0cddd38b6c9a36f9f496e9bbfdae1962d
sboikov <sboikov@gridgain.com>
2015-10-27 15:57:28 +0300

    ignite-1607 WIP

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/near/GridNearOptimisticSerializableTxPrepareFuture.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/near/GridNearOptimisticSerializableTxPrepareFuture.java
@@ -444,7 +444,8 @@ public class GridNearOptimisticSerializableTxPrepareFuture extends GridNearOptim
             m.explicitLock(),
             tx.subjectId(),
             tx.taskNameHash(),
-            m.clientFirst());
+            m.clientFirst(),
+            tx.activeCachesDeploymentEnabled());
 
         for (IgniteTxEntry txEntry : m.writes()) {
             if (txEntry.op() == TRANSFORM)
